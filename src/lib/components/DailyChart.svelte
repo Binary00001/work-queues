5371fb37916095d24d0c747b57045d6feb34d5f3
Andrew Riggs <61713590+Binary00001@users.noreply.github.com>
2021-10-28 15:25:19 -0700

    10/28/21

--- src/lib/components/DailyChart.svelte
+++ src/lib/components/DailyChart.svelte
@@ -1,51 +1,48 @@
 <script>
-    import Line from 'svelte-chartjs/src/Line.svelte'
-    export let labels 
-    export let data
-    export let dailyGoal
+	import Line from 'svelte-chartjs/src/Line.svelte';
+	export let labels;
+	export let data;
+	export let dailyGoal;
 
-    let dataLine = {
-        labels,
-        datasets: [
-            {
-                label: 'Jobs Completed',
-                fill: false,
-                lineTension: 0.3,
-                backgroundColor: "rgba(225, 204,230, .3)",
-                borderColor: "rgb(205, 130, 158)",
-                borderCapStyle: "butt",
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: "miter",
-                pointBorderColor: "rgb(205, 130,1 58)",
-                pointBackgroundColor: "rgb(255, 255, 255)",
-                pointBorderWidth: 10,
-                pointHoverRadius: 5,
-                pointHoverBackgroundColor: "rgb(0, 0, 0)",
-                pointHoverBorderColor: "rgba(220, 220, 220,1)",
-                pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 10,
-                data
-            }, 
-            {
-                label: '',
-                fill: false,
-                borderColor: "rgba(0, 0, 0, 0)",
-                data: [0, 80]
-            },
-            {
-                label: 'Daily Goal',
-                lineTension: 0.3,
-                fill: false,
-                borderColor: 'rgba(158, 130, 255, .8)',
-                data: dailyGoal
-            }
-        ]
-    }
+	let dataLine = {
+		labels,
+		datasets: [
+			{
+				label: 'Jobs Completed',
+				fill: false,
+				lineTension: 0.3,
+				backgroundColor: 'rgba(225, 204,230, .3)',
+				borderColor: 'rgb(205, 130, 158)',
+				borderCapStyle: 'butt',
+				borderDash: [],
+				borderDashOffset: 0.0,
+				borderJoinStyle: 'miter',
+				pointBorderColor: 'rgb(205, 130,1 58)',
+				pointBackgroundColor: 'rgb(255, 255, 255)',
+				pointBorderWidth: 10,
+				pointHoverRadius: 5,
+				pointHoverBackgroundColor: 'rgb(0, 0, 0)',
+				pointHoverBorderColor: 'rgba(220, 220, 220,1)',
+				pointHoverBorderWidth: 2,
+				pointRadius: 1,
+				pointHitRadius: 10,
+				data
+			},
+			{
+				label: '',
+				fill: false,
+				borderColor: 'rgba(0, 0, 0, 0)',
+				data: [0, 80]
+			},
+			{
+				label: 'Daily Goal',
+				lineTension: 0.3,
+				fill: false,
+				borderColor: 'rgba(158, 130, 255, .8)',
+				data: dailyGoal
+			}
+		]
+	};
 </script>
-        <Line data={dataLine} options={{ responsive: true }} />
 
-<style>
-
-</style>
+<Line data={dataLine} options={{ responsive: true }} />
